8c8b88d15825f75866b004e4c885dde4bcf9c82e
jasspn2014 <jasspn2014@gmail.com>
2019-11-17 18:33:26 +0530

    Camera Permission at the Time of App Startup

--- app/src/main/java/com/aroradevelopers/qrgenscan/SplashScreen.java
+++ app/src/main/java/com/aroradevelopers/qrgenscan/SplashScreen.java
@@ -1,10 +1,19 @@
 package com.aroradevelopers.qrgenscan;
 
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class SplashScreen extends Activity {
 
@@ -12,15 +21,32 @@ public class SplashScreen extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen_activity);
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.CAMERA)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        Handler handler = new Handler();
 
-        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable(){
+                            @Override
+                            public void run() {
+                                startMainActivity();
+                            }
+                        },1500);
+                    }
 
-        handler.postDelayed(new Runnable(){
-            @Override
-            public void run() {
-                startMainActivity();
-            }
-        },1500);
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response) {
+                        Toast.makeText(SplashScreen.this,"You Must Enable Camera Permission",Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+
+                    }
+                }).check();
+
     }
 
     private void startMainActivity() {
